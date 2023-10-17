1c67759e6777e328094413e88cb249c890a855f7
Oleg Labyr <112154900+Samadhi555@users.noreply.github.com>
2023-10-17 19:11:44 +0300

    new Commit_1

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,30 @@
 package jm.task.core.jdbc.util;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+    public static Connection getMySQLConnection() {
+        String hostName = "localhost";
+        String dbName = "mybdtest";
+        String userName = "root";
+        String password = "Oleg5577";
+        try {
+            return getMySQLConnection(hostName, dbName, userName, password);
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Connection getMySQLConnection(String hostName, String dbName,   String userName, String password) throws SQLException,   ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        String connectionURL = "jdbc:mysql://" + hostName + ":3306/" + dbName;
+        return (Connection) DriverManager.getConnection(connectionURL, userName, password);
+
+
+
+    }
+
 }
+
